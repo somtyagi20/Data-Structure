bcb605385f8ef0afa22d6eff5dae0a9a2265577f
somtyagi20 <102314050+somtyagi20@users.noreply.github.com>
2022-11-21 15:14:04 +0530

    Update Incomplete_Stack_Using_Array.c

--- Incomplete_Stack_Using_Array.c
+++ Incomplete_Stack_Using_Array.c
@@ -1,69 +1,82 @@
 //-------------STACK USING ARRAY---------------
 #include<stdio.h>
 #include<stdlib.h>
-void push(int stack[],int n,int top){
+int stack[100],top=-1,n;
+void create(){
+    printf("Enter the size of the stack : ");
+    scanf("%d",&n);
+}
+int push(int top){
     int s,t;
     printf("How many elements You want to push :");
     scanf("%d",&s);
     t=top+s;
-    for(int i=top+1;i<t;i++){
+    for(int i=top+1;i<=t;i++){
      if(i>=n){
 	    printf(" Stack Overflow !");
 	    break;
 	 }else{
 	    top++;
+	    printf("Enter value : ");
 	    scanf("%d",&stack[i]);
 	}
     }
-	
+	return top;
 }
-void pop(int stack[],int n,int top){
+int pop(int top){
     if(top<=-1){
         printf("Stack Underflow");
     }else{
         top--;
     }
-	
+	return top;
 }
-void peek(int stack[],int n,int top){
-	printf("Top element of Stack is : %d",stack[top]);
+void peek(int top){
+	printf("\n Top element of Stack is : %d \n ",stack[top]);
 }
-void display(int stack[],int n,int top){
-	for(int i=0;i<top;i++){
-	    printf("%d \n",stack[i]);
+void display(int top){
+    if(top==-1){
+        printf("Stack is empty !");
+    }else{
+        printf("Stack : ");
+	for(int i=0;i<=top;i++){
+	    printf("%d,",stack[i]);
 	}
+	printf("\n\n");
+    }
 }
 int main(){
-    int c,n;
-	printf("-----------------STACK----------------");
-	printf("\n --------Creating Stack--------  \n");
-	printf("Enter the size of stack :");
-	scanf("%d",&n);
-	int stack[n],top=-1;
-	printf("\n ~Choose Operation -\n");
-	printf("\n 1.Push ");
-	printf("\n 2.Pop ");
-	printf("\n 3.Peek ");
-	printf("\n 4.Display ");
-	printf("\n 5.Quit \n");
-	printf("\n Enter Your Choice : ");
-	scanf("%d",&c);
-
-	while(1){
-		switch(c){
+    int c;
 	
-		case 1: push(stack[],n,top);
+	while(1){
+	    printf("-----------------STACK----------------\n");
+	    printf("\n ~Choose Operation -\n");
+	    printf("\n 1.Create Stack ");
+	    printf("\n 2.Push ");
+	    printf("\n 3.Pop ");
+	    printf("\n 4.Peek ");
+	    printf("\n 5.Display ");
+	    printf("\n 6.Quit \n");
+	
+	    printf("\n Enter Your Choice : ");
+	    scanf("%d",&c);
+		switch(c){
+		    
+		case 1: create();
 		        break;
-		case 2: pop(stack[],n,top);
+	
+		case 2: top=push(top);
 		        break;
-		case 3: peek(stack[],n,top);
+		case 3: top=pop(top);
 		        break;
-		case 4: display(stack[],n,top);
+		case 4: peek(top);
 		        break;
-		case 5: exit(1);
+		case 5: display(top);
+		        break;
+		case 6: exit(1);
 		        break;
 		        
-		default: break;
+		default: printf("Enter correct choice !");
 	}
 	}
   return 0;
